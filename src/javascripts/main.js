18da8cd76f519f82a0286c304c33bb473e37aec5
Austin Phy <atphy42@gmail.com>
2020-07-06 11:47:04 -0500

    added login functionality

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,16 @@
 import 'bootstrap';
 import '../styles/main.scss';
 
-console.error('hi');
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
+  auth.loginButton();
+};
+
+init();
