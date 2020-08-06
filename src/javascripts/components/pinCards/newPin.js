6e44c2fd15958d0bbca034434d0f3de01c198978
Austin Phy <atphy42@gmail.com>
2020-08-06 17:26:33 -0500

    adding new pins now works

---
+++ src/javascripts/components/pinCards/newPin.js
@@ -0,0 +1,20 @@
+import pinData from '../../helpers/data/pins';
+import pinCards from './pinCards';
+
+const addNewPin = () => {
+  const newPinObj = {
+    name: $('#pinName').val(),
+    image: $('#pinImg').val(),
+    url: $('pinLink').val(),
+  };
+  pinData.addNewPin(newPinObj)
+    .then(() => {
+      pinCards.buildPins();
+    });
+};
+
+const newPinSelector = () => {
+  $('#submit-pin').click(addNewPin);
+};
+
+export default { newPinSelector };
